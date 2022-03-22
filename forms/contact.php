3cd7aff709a1432412966872e94facbdc6bf2acc
Arnetta <40047791+ark229@users.noreply.github.com>
2022-03-22 09:45:10 -0400

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -8,7 +8,7 @@
  */
 
 // Replace contact@example.com with your real receiving email address
-$receiving_email_address = 'knightdevonline@gmail.com';
+$receiving_email_address = 'admin@knightdevonline.com';
 
 if (file_exists($php_email_form = '../Imperial/assets/vendor/php-email-form/php-email-form.php')) {
   include($php_email_form);
@@ -25,14 +25,14 @@ $contact->from_email = $_POST['email'];
 $contact->subject = $_POST['subject'];
 
 //Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-
+/* 
 $contact->smtp = array(
   'host' => 'https://gmail.com',
   'username' => 'knightdevonline',
   'password' => 'Ar013511!',
   'port' => '587'
 );
-
+*/
 
 $contact->add_message($_POST['name'], 'From');
 $contact->add_message($_POST['email'], 'Email');
