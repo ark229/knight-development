6b98d955510745bc4159bd6a768355cac5b32a2b
Arnetta <40047791+ark229@users.noreply.github.com>
2022-03-21 20:06:24 -0400

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -24,15 +24,15 @@ $contact->from_name = $_POST['name'];
 $contact->from_email = $_POST['email'];
 $contact->subject = $_POST['subject'];
 
-// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-/*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+//Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+
+$contact->smtp = array(
+  'host' => 'gmail.com',
+  'username' => 'knightdevonline',
+  'password' => 'Ar013511!',
+  'port' => '587'
+);
+
 
 $contact->add_message($_POST['name'], 'From');
 $contact->add_message($_POST['email'], 'Email');
